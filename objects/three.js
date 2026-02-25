0cc379087bc07c4a85e2692cccbe82019edd6158
Sanket Surve <sanketsurve2756@gmail.com>
2026-02-25 12:52:47 +0530

    merge two object in array

--- objects/three.js
+++ objects/three.js
@@ -1,7 +1,26 @@
-let obj1 = new Object();
-obj1.id = "101";
-obj1.email = "sanket@gmail.com";
-obj1.password = 1234;
-obj1.isLogged = true;
+let obj1 = {
+    id: "101",
+    email: "sanket@gmail.com",
+    password: 1234,
+    isLogged: true,
+    greeting: function(){
+        console.log("Hello Sanket");
+        return 0;
+    }
+};
 
-console.log(obj1);
+let obj2 = {
+    id: "102",
+    email: "Viraj@gmail.com",
+    password: 1234,
+    isLogged: true,
+    greeting: function(){
+        console.log("Hello Viraj");
+        return 0;
+    }
+}
+
+let obj3 = [obj1,obj2];
+console.log(obj3);
+
+
